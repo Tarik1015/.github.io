504ca6f281beebe7ed19af689be20e89ac097759
Tarik1015 <57346434+Tarik1015@users.noreply.github.com>
2019-11-06 13:28:54 +0100

    eerste versie van de website

---
+++ project-portfolio-Tarik1015/javascript.js
@@ -0,0 +1,13 @@
+<SCRIPT TYPE="text/javascript" LANGUAGE="JavaScript">
+<!--
+var nu = new Date();
+var dag = nu.getDate();
+var dag2 = ((dag < 10) ? "0" : "") + dag;
+var maand = nu.getMonth() + 1;
+var maand2 = ((maand < 10) ? "0" : "") + maand;
+var jaar = nu.getYear();
+var jaar4 = ((jaar < 1900) ? (jaar + 1900) : (jaar));
+
+document.write(dag2 + "-" + maand2 + "-" + jaar4);
+//-->
+</SCRIPT>
